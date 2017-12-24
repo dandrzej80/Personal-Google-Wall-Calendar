3da0fec21b273c9de2bf40455de760328df7f703
dandrzej80 <dandrzej80@gmail.com>
2017-12-24 14:31:00 -0500

    Update page styling

--- setup.sh
+++ setup.sh
@@ -14,12 +14,11 @@ sudo apt-get install apache2 -y
 
 #Move Webpage
 cd
-sudo mv /Google-Wall-Calendar/index.html /var/www/html/index.html
+sudo mv ./Google-Wall-Calendar/index.html /var/www/html/index.html
 
 #Install Web Browser and Customize Page
 sudo apt-get install iceweasel -y
-sudo mv /Google-Wall-Calendar/index.html /var/www/html/index.html
-#sudo mv /Google-Wall-Calendar/stylish.css 
+sudo mv ./Google-Wall-Calendar/stylish.css /var/www/html/stylish.css 
 
 #Clean up Install Files
 #Remove Raspi-Scripts Folder
