e53c54d575a4169f4925486cbad760d29856df01
dandrzej80 <dandrzej80@gmail.com>
2017-12-31 11:44:09 -0500

    Setup Changes

--- startkiosk.sh
+++ startkiosk.sh
@@ -1,4 +1,2 @@
-sleep 15
-iceweasel file:///var/www/html/index.html
-sleep 15
-xdotool search --onlyvisible --name iceweasel key F11
+iceweasel -url file:///var/www/html/index.html &
+xdotool search --onlyvisible --name "iceweasel" windowactivate key F11
