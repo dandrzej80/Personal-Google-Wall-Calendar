5cba8eea3f0cfb63646a6757c2e00e36f525c73d
dandrzej80 <dandrzej80@gmail.com>
2017-12-31 11:59:13 -0500

    Setup Changes

--- setup.sh
+++ setup.sh
@@ -17,7 +17,8 @@ cd
 sudo mv /home/pi/Google-Wall-Calendar/index.html /var/www/html/index.html
 
 #Install Web Browser and Customize Page
-sudo apt-get install iceweasel unclutter x11-xserver-utils xdotool -y
+sudo apt-get install iceweasel unclutter x11-xserver-utils -y
+sudo apt-get install xdotool
 sudo mv /home/pi/Google-Wall-Calendar/stylish.css /var/www/html/stylish.css
 sudo mv /home/pi/Google-Wall-Calendar/startkiosk.sh /var/www/html/startkiosk.sh
 cat /home/pi/Google-Wall-Calendar/edit-autostart | sudo tee -a /home/pi/.config/lxsession/LXDE-pi/autostart
